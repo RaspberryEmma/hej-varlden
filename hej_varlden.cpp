47f98fdd2944b4b8f24ebc8312a5aee95a521c1c
Emma <91637007+RaspberryEmma@users.noreply.github.com>
2021-09-29 23:45:08 +0100

    Update and rename hello.cpp to hej_varlden.cpp

---
+++ hej_varlden.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+
+// A "hello world" program featuring multiple languages!
+
+int main()
+{
+	cout << "hello, world!" << endl ;
+	cout << "hej, varlden!" << endl ;
+	return 0 ;
+}

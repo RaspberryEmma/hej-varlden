f01a94c51d6651371352ea389d60abc77b338a85
Emma <91637007+RaspberryEmma@users.noreply.github.com>
2021-09-29 23:40:45 +0100

    Update Test.hs

--- Test.hs
+++ Test.hs
@@ -1,4 +1,7 @@
-main = do putStrLn "Hello World!  What is the square root of 16?"
+main = do putStrLn "hello, world!"
+          putStrLn "hej, varlden!"
+          
+          putStrLn "What is the square root of 16?"
           x <- readLn
           if x == 4
               then putStrLn "True!"

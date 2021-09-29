36001f03f59b92df74760551b004c6066703f00a
Emma <91637007+RaspberryEmma@users.noreply.github.com>
2021-09-29 23:35:20 +0100

    Update Hello.java
    Add Swedish translation

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Hello
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Hello World!");
+		System.out.println("hello, world!");
+		System.out.println("hej, varlden!");
 	}
 }

75681436c5847de36d860dcfd5e23ea8a883e9e6
Emma <91637007+RaspberryEmma@users.noreply.github.com>
2021-09-29 23:30:32 +0100

    Create hej-varlden.py
    Displays "hello, world!" in both English and Swedish.

---
+++ hej-varlden.py
@@ -0,0 +1,12 @@
+# A "Hello world" program written in python
+# Emma Tarmey
+
+def message_in_english():
+	print("hello, world!")
+
+def message_in_swedish():
+	# TODO: add umlaut
+	print("hej, varlden!")
+
+message_in_english()
+message_in_swedish()
